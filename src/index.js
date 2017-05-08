e09cb2206e8a0b15467a521b7414d1a59f7718fa
linneaholm <a.linnea.holm@gmail.com>
2017-05-08 15:01:56 +0200

    kartan

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import CanvasMap from './canvas-map'
 let map = CanvasMap({
     textContainer: document.querySelector('.text'),
     mapSrc: 'img/map.svg',
-    trailVisitedColor: '#47DBB4',
-    fontPresentColor: '#5D5C56',
+    trailVisitedColor: '#db4695',
+    fontPresentColor: '#3d3938',
   })
   .appendTo('.container')
